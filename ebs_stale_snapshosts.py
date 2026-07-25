7db15f0235163a573a1268d61f71fc3daeb2cd07
Atri Ghosh <137553942+Atri9Ghosh@users.noreply.github.com>
2026-07-25 07:58:46 +0530

    Implement deletion of unattached EBS snapshots
    Added logic to delete EBS snapshots not attached to any volume.

--- ebs_stale_snapshosts.py
+++ ebs_stale_snapshosts.py
@@ -1,24 +1,18 @@
 import boto3
-
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
-
     # Get all EBS snapshots
     response = ec2.describe_snapshots(OwnerIds=['self'])
-
     # Get all active EC2 instance IDs
     instances_response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     active_instance_ids = set()
-
     for reservation in instances_response['Reservations']:
         for instance in reservation['Instances']:
             active_instance_ids.add(instance['InstanceId'])
-
     # Iterate through each snapshot and delete if it's not attached to any volume or the volume is not attached to a running instance
     for snapshot in response['Snapshots']:
         snapshot_id = snapshot['SnapshotId']
         volume_id = snapshot.get('VolumeId')
-
         if not volume_id:
             # Delete the snapshot if it's not attached to any volume
             ec2.delete_snapshot(SnapshotId=snapshot_id)
